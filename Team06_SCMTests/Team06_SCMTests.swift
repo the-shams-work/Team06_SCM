abdef175c0d44ad07fe980bceef2d4bae1854e41
Shams Tabrej Alam <shams.22scse1010782@galgotiasuniversity.edu.in>
2024-12-02 12:12:42 +0530

    Initial Commit

---
+++ Team06_SCMTests/Team06_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team06_SCMTests.swift
+//  Team06_SCMTests
+//
+//  Created by Shams Tabrej Alam on 02/12/24.
+//
+
+import Testing
+@testable import Team06_SCM
+
+struct Team06_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
